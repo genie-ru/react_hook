deb4df4294d90993ab996bfb2bfb95d79f43c7e4
genie-ru <genie.zgmf987@gmail.com>
2023-06-06 19:31:35 +0900

    add hook

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import React,{ useState } from 'react'
 
 const App = ({initialCount}) => {
     let [state, setState] = useState({
-        count: initialCount,
-        user: "Francis",
-        });
-
-
+        count:initialCount,
+        user:"Francis"
+    });
     let [posts,setPosts] = useState([
         {
-            name:"Super awesome hooks", 
-            body: "Everying you need for your hooks"
+            name:"Super awesome hooks",
+            body:"Everything you need for your hooks"
         }
     ]);
 
+
+
+    // const addOne = () => {
+    //     setCount(count + 1)
+    // }
+
     const restOne = () => {
         setState( prevState => { 
-            return { ...prevState, count: prevState.count - 1 }
+           return { ...prevState, count: prevState.count - 1 }
         })
     }
 
@@ -32,31 +36,28 @@ const App = ({initialCount}) => {
             newPost
         ]);
     }
-    // const addOne = () => {
-    //     setCount(count + 1)
-    // }
+
 
     return(
         <>
             <h1>{state.user}</h1>
             <h3>Count:{state.count}</h3>
-            <button onClick={ () => setState( {...state, count: state.count + 1} )}>Add one +1</button>
+            <button onClick={ () => setState({...state,count: state.count + 1 })}>Add one +1</button>
             <button onClick={ restOne }>Rest one -1</button>
             <button onClick={ () => setState(
-                {...state, count: initialCount }
+                {...state,count: initialCount }
             ) }>RESET</button>
 
             <hr/>
-            <button onClick={addOnePost}>Add post</button>
+            <button onClick={addOnePost}>Add one more</button>
             { posts.map((item,i)=>(
-                    <div key={i}>
+                <div key={i}>
                     <div>Name:{item.name}</div>
                     <div>Body:{item.body}</div>
                     <hr/>
                 </div>
             ))}
-            {/* <button onClick={addOnePost}>Add post</button> */}
-
+            
         </>
     )
 }
