8882d34b5a35868c205640891c40cceeed6dff36
genie-ru <genie.zgmf987@gmail.com>
2023-06-06 16:46:42 +0900

    add react_hook

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React,{ useState } from 'react'
+
+
+const App = ({initialCount}) => {
+    let [count, setCount] = useState(initialCount);
+
+    // const addOne = () => {
+    //     setCount(count + 1)
+    // }
+
+    const restOne = () => {
+        setCount( prevCount => { 
+            return prevCount - 1
+        })
+    }
+
+    return(
+        <>
+            <h3>Count:{count}</h3>
+            <button onClick={ () => setCount( count + 1 )}>Add one +1</button>
+            <button onClick={ restOne }>Rest one -1</button>
+            <button onClick={ () => setCount(initialCount) }>RESET</button>
+        </>
+    )
+}
+
+export default App;
