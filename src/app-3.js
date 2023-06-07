1244ecc4d5db9686ed18165c8d269c230bf12e3c
genie-ru <genie.zgmf987@gmail.com>
2023-06-07 21:33:57 +0900

    fixed app-3.js

--- src/app-3.js
+++ src/app-3.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React,{ useRef } from 'react';
 
-
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.textInput = React.createRef();
+const App = () => {
+    const textInput = useRef();
+    const triggerHandler = () => {
+        console.log(textInput.current.value);
     }
 
-    triggerHandler = () => {
-        console.log(this.textInput.current.value);
-    }
-
-    render() {
-        return (
-            <>
-                <h1>Form:</h1>
-                <input type="text" ref={this.textInput} />
-                <button
-                    onClick={this.triggerHandler}
-                >
-                    Trigger
-                </button>
-            </>
-        );
-    }
+    return (
+        <>
+            <h1>Form:</h1>
+            <InputComponent  ref={textInput} />
+            <button
+                onClick={triggerHandler}
+            >
+                Trigger
+            </button>
+        </>
+    );
+    
 }
+
+const InputComponent = React.forwardRef((props, ref) => {
+    return <input type="text" ref={ref}/>
+})
 
 export default App;
